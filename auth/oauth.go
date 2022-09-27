0c9b4e82abb0f3b272b54bd803cd25145f09299d
grandeto <grandeto.com@gmail.com>
2022-09-28 00:45:14 +0300

    process auth through env vars

--- auth/oauth.go
+++ auth/oauth.go
@@ -2,10 +2,12 @@ package auth
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"time"
+
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-	"net/http"
-	"time"
 )
 
 type authCodeFn func(string) func() string
@@ -23,7 +25,10 @@ func NewFileSourceClient(clientId, clientSecret, tokenFile string, authFn authCo
 	// or refresh token is missing
 	if !exists || token.RefreshToken == "" {
 		authUrl := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-		authCode := authFn(authUrl)()
+		authCode := os.Getenv("AUTH_CODE")
+		if authCode == "" {
+			authCode = authFn(authUrl)()
+		}
 		token, err = conf.Exchange(oauth2.NoContext, authCode)
 		if err != nil {
 			return nil, fmt.Errorf("Failed to exchange auth code for token: %s", err)
@@ -67,16 +72,16 @@ func NewAccessTokenClient(clientId, clientSecret, accessToken string) *http.Clie
 
 func NewServiceAccountClient(serviceAccountFile string) (*http.Client, error) {
 	content, exists, err := ReadFile(serviceAccountFile)
-	if(!exists) {
+	if !exists {
 		return nil, fmt.Errorf("Service account filename %q not found", serviceAccountFile)
 	}
 
-	if(err != nil) {
+	if err != nil {
 		return nil, err
 	}
 
 	conf, err := google.JWTConfigFromJSON(content, "https://www.googleapis.com/auth/drive")
-	if(err != nil) {
+	if err != nil {
 		return nil, err
 	}
 	return conf.Client(oauth2.NoContext), nil
